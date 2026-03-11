ccb10a88d93f153ffe5735ce1e1847d44740bb39
Ajaykumar47 <gunthapallyajaykumar47@gmail.com>
2026-03-11 23:30:11 +0530

    commit-code 2

---
+++ DecrementTillOne.java
@@ -0,0 +1,17 @@
+package com.java.fundamentals;
+
+import java.util.Scanner;
+
+public class DecrementTillOne {
+    public static void main(String[] args) {
+        Scanner Scanner =new Scanner(System.in);
+        System.out.println("Enter tha value of N");
+        int N = Scanner.nextInt();
+
+        for (int i = N; i >=1 ; i--) {
+            System.out.println("i"+ i);
+Scanner.close();
+
+    }
+}
+}
